4a473c9b4e63a5a267eab7ab76fce95d82eab24c
Rita <rita@dyn-129-236-238-235.dyn.columbia.edu>
2019-02-09 09:47:24 -0500

    added hw

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+$( document ).ready(function() {
+
+
+var counter = 0;
+var randNum = "";
+var wins = 0;
+var loses = 0;
+randNum = Math.floor(Math.random() * (120 - 19) +19);
+
+$("#random-number").html(randNum);
+
+
+$(".crystal-image").on("click", function(){
+
+   counter += Math.floor(Math.random() * (12 - 1) + 1);
+ $("#total-score").html(counter);
+   
+
+   if (counter === randNum) {
+       wins ++;
+       $("#exclamation").show();
+       counter = 0;
+   
+   } else if (counter > randNum) {
+    loses ++;
+    counter = 0;
+    $("#exclamation").hide();
+    randNum = Math.floor(Math.random() * (120 - 19) +19);
+    $("#random-number").html(randNum);
+
+   }
+
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("loses").innerHTML = loses;
+});
+
+});
+
+
+
+
